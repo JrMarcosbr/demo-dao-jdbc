63bdeda2676535484fb4b2f0364aeb1215ac14a7
MarcosJr <marcosjorgejr1@gmail.com>
2022-11-29 18:21:54 -0300

    Adc Seller

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,17 @@
 package application;
 
+import java.util.Date;
+
 import model.entities.Departament;
+import model.entities.Seller;
 
 public class Program {
 	public static void main(String args[]){
 		Departament obj = new Departament(1, "Books");
 		System.out.println(obj);
+		
+		Seller seller = new Seller(21, "Marcos","emal@gmail.com", new Date(), 3000,obj);
+		
+		System.out.println(seller);
 	}
 }
